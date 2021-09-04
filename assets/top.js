0b8ed2f7a1f18e678527351e808113e3f58d8c2e
lakshmipooja_ramesh <lakshmipooja.ramesh@solitontech.com>
2021-09-04 10:18:50 +0530

    feat: added inheritance using ES6 classes

--- assets/top.js
+++ assets/top.js
@@ -128,141 +128,142 @@ function find_index(city_val){
   }
   return index;
 }
-function parent_index(_city_)
-{
-  this._city_ = _city_;
+class parent_index {
+  constructor(_city_) {
+    this._city_ = _city_
+  }
+  print_city() {
+    ind = find_index(this._city_)
+    document.getElementById('temp1').innerHTML = temperature_array[ind]
+    var fahr = celToFahr(temperature_array[ind])
+    document.getElementById('temp2').innerHTML = fahr + " F"
+    document.getElementById('humid1').innerHTML = humid_array[ind]
+    document.getElementById('precip1').innerHTML = precip_array[ind]
+    var dAndT = dAndT_array[ind]
+    document.getElementById('date').innerHTML = dAndT.substr(0, dAndT.indexOf(','))
+    document.getElementById('time1').innerHTML = dAndT.substr(dAndT.indexOf(',') + 1, dAndT.length)
+    var hr1 = next_five_hrs_array[ind][0]
+    var hr2 = next_five_hrs_array[ind][1]
+    var hr3 = next_five_hrs_array[ind][0]
+    var hr4 = next_five_hrs_array[ind][0]
+    const _img2 = document.getElementById('img2')
+    iconChange(temperature_array[ind], _img2)
+    const _img3 = document.getElementById('img3')
+    iconChange(hr1, _img3)
+    const _img4 = document.getElementById('img4')
+    iconChange(hr2, _img4)
+    const _img5 = document.getElementById('img5')
+    iconChange(hr3, _img5)
+    const _img6 = document.getElementById('img6')
+    iconChange(hr4, _img6)
+    document.getElementById('now').innerHTML = 'NOW'
+    document.getElementById('cloud0').innerHTML = temperature_array[ind]
+    document.getElementById('cloud1').innerHTML = hr1
+    document.getElementById('cloud2').innerHTML = hr2
+    document.getElementById('cloud3').innerHTML = hr3
+    document.getElementById('cloud4').innerHTML = hr4
+    var c1 = parseInt(dAndT.substr(dAndT.indexOf(',') + 1, dAndT.length))
+    var c2
+    var c3
+    var c4
+    var c5
+    var am_or_pm = dAndT.substr(dAndT.length - 2, dAndT.length)
+    if (c1 < 9) {
+      c2 = c1 + 1
+      c3 = c1 + 2
+      c4 = c1 + 3
+      c5 = c1 + 4
+    }
+    else if (c1 == 9) {
+      c2 = 10
+      c3 = 11
+      c4 = 12
+      c5 = 1
+    }
+    else if (c1 == 10) {
+      c2 = 11
+      c3 = 12
+      c4 = 1
+      c5 = 2
+    }
+    else if (c1 == 11) {
+      c2 = 12
+      c3 = 1
+      c4 = 2
+      c5 = 3
+    }
+    else if (c1 == 12) {
+      c2 = 1
+      c3 = 2
+      c4 = 3
+      c5 = 4
+    }
+    const img1 = document.getElementById('icon-change')
+    img1.setAttribute('src', './././img/' + this._city_.toLowerCase() + '.svg')
+    if (c2 == 1 && am_or_pm == 'AM') {
+      document.getElementById('t1').innerHTML = c2 + " " + 'PM'
+      document.getElementById('t2').innerHTML = c3 + " " + 'PM'
+      document.getElementById('t3').innerHTML = c4 + " " + 'PM'
+      document.getElementById('t4').innerHTML = c5 + " " + 'PM'
+    }
+    else if (c2 == 1 && am_or_pm == 'PM') {
+      document.getElementById('t1').innerHTML = c2 + " " + 'AM'
+      document.getElementById('t2').innerHTML = c3 + " " + 'AM'
+      document.getElementById('t3').innerHTML = c4 + " " + 'AM'
+      document.getElementById('t4').innerHTML = c5 + " " + 'AM'
+    }
+    else if (c3 == 1 && am_or_pm == 'PM') {
+      document.getElementById('t1').innerHTML = c2 + " " + 'PM'
+      document.getElementById('t2').innerHTML = c3 + " " + 'AM'
+      document.getElementById('t3').innerHTML = c4 + " " + 'AM'
+      document.getElementById('t4').innerHTML = c5 + " " + 'AM'
+    }
+    else if (c3 == 1 && am_or_pm == 'AM') {
+      document.getElementById('t1').innerHTML = c2 + " " + 'AM'
+      document.getElementById('t2').innerHTML = c3 + " " + 'PM'
+      document.getElementById('t3').innerHTML = c4 + " " + 'PM'
+      document.getElementById('t4').innerHTML = c5 + " " + 'PM'
+    }
+    else if (c4 == 1 && am_or_pm == 'AM') {
+      document.getElementById('t1').innerHTML = c2 + " " + 'AM'
+      document.getElementById('t2').innerHTML = c3 + " " + 'AM'
+      document.getElementById('t3').innerHTML = c4 + " " + 'PM'
+      document.getElementById('t4').innerHTML = c5 + " " + 'PM'
+    }
+    else if (c4 == 1 && am_or_pm == 'PM') {
+      document.getElementById('t1').innerHTML = c2 + " " + 'PM'
+      document.getElementById('t2').innerHTML = c3 + " " + 'PM'
+      document.getElementById('t3').innerHTML = c4 + " " + 'AM'
+      document.getElementById('t4').innerHTML = c5 + " " + 'AM'
+    }
+    else if (c5 == 1 && am_or_pm == 'AM') {
+      document.getElementById('t1').innerHTML = c2 + " " + 'AM'
+      document.getElementById('t2').innerHTML = c3 + " " + 'AM'
+      document.getElementById('t3').innerHTML = c4 + " " + 'AM'
+      document.getElementById('t4').innerHTML = c5 + " " + 'PM'
+    }
+    else if (c5 == 1 && am_or_pm == 'PM') {
+      document.getElementById('t1').innerHTML = c2 + " " + 'PM'
+      document.getElementById('t2').innerHTML = c3 + " " + 'PM'
+      document.getElementById('t3').innerHTML = c4 + " " + 'PM'
+      document.getElementById('t4').innerHTML = c5 + " " + 'AM'
+    }
+    else {
+      document.getElementById('t1').innerHTML = c2 + " " + am_or_pm
+      document.getElementById('t2').innerHTML = c3 + " " + am_or_pm
+      document.getElementById('t3').innerHTML = c4 + " " + am_or_pm
+      document.getElementById('t4').innerHTML = c5 + " " + am_or_pm
+    }
+  }
 }
-function child_index(_city_)
-{
-  parent_index.call(this, _city_);
+class child_index extends parent_index{
+  constructor(_city_){
+    super();
+  }
 }
 var ind;
-parent_index.prototype.print_city = function(){
-  ind = find_index(this._city_);
-  console.log(this._city_ + " " + ind);
-  document.getElementById('temp1').innerHTML = temperature_array[ind];
-  var fahr = celToFahr(temperature_array[ind]);
-  document.getElementById('temp2').innerHTML = fahr;
-  document.getElementById('humid1').innerHTML = humid_array[ind];
-  document.getElementById('precip1').innerHTML=precip_array[ind];
-  var dAndT = dAndT_array[ind];
-  document.getElementById('date').innerHTML = dAndT.substr(0, dAndT.indexOf(','));
-  document.getElementById('time1').innerHTML = dAndT.substr(dAndT.indexOf(',')+1, dAndT.length);
-  var hr1 = next_five_hrs_array[ind][0];
-  var hr2 = next_five_hrs_array[ind][1];
-  var hr3 = next_five_hrs_array[ind][0];
-  var hr4 = next_five_hrs_array[ind][0];
-  const _img2 = document.getElementById('img2')
-  iconChange(temperature_array[ind], _img2)
-  const _img3 = document.getElementById('img3')
-  iconChange(hr1, _img3)
-  const _img4 = document.getElementById('img4')
-  iconChange(hr2, _img4)
-  const _img5 = document.getElementById('img5')
-  iconChange(hr3, _img5)
-  const _img6 = document.getElementById('img6')
-  iconChange(hr4, _img6)
-  document.getElementById('now').innerHTML = 'NOW'
-  document.getElementById('cloud0').innerHTML = temperature_array[ind]
-  document.getElementById('cloud1').innerHTML = hr1
-  document.getElementById('cloud2').innerHTML = hr2
-  document.getElementById('cloud3').innerHTML = hr3
-  document.getElementById('cloud4').innerHTML = hr4
-  var c1 = parseInt(dAndT.substr(dAndT.indexOf(',')+1, dAndT.length))
-  var c2;
-  var c3;
-  var c4;
-  var c5;
-  var am_or_pm = dAndT.substr(dAndT.length-2, dAndT.length);
-  if(c1 < 9){
-    c2 = c1 + 1;
-    c3 = c1 + 2;
-    c4 = c1 + 3;
-    c5 = c1 + 4;
-  }
-  else if(c1 == 9){
-    c2 = 10;
-    c3 = 11;
-    c4 = 12;
-    c5 = 1;
-  }
-  else if(c1 == 10){
-    c2 = 11;
-    c3 = 12;
-    c4 = 1;
-    c5 = 2;
-  }
-  else if(c1 == 11){
-    c2 = 12;
-    c3 = 1;
-    c4 = 2;
-    c5 = 3;
-  }
-  else if(c1 == 12){
-    c2 = 1;
-    c3 = 2;
-    c4 = 3;
-    c5 = 4;
-  }
-  const img1 = document.getElementById('icon-change')
-  img1.setAttribute('src', './././img/'+this._city_.toLowerCase()+'.svg')
-  if(c2 == 1 && am_or_pm == 'AM'){
-    document.getElementById('t1').innerHTML = c2 + " " + 'PM'
-    document.getElementById('t2').innerHTML = c3 + " " + 'PM'
-    document.getElementById('t3').innerHTML = c4 + " " + 'PM'
-    document.getElementById('t4').innerHTML = c5 +  " " +'PM'
-  }
-  else if(c2 == 1 && am_or_pm == 'PM'){
-    document.getElementById('t1').innerHTML = c2 + " " + 'AM'
-    document.getElementById('t2').innerHTML = c3 + " " + 'AM'
-    document.getElementById('t3').innerHTML = c4 + " " + 'AM'
-    document.getElementById('t4').innerHTML = c5 +  " " +'AM'
-  }
-  else if(c3 == 1 && am_or_pm == 'PM'){
-    document.getElementById('t1').innerHTML = c2 +  " " +'PM'
-    document.getElementById('t2').innerHTML = c3 +  " " +'AM'
-    document.getElementById('t3').innerHTML = c4 +  " " +'AM'
-    document.getElementById('t4').innerHTML = c5 +  " " +'AM'
-  }
-  else if(c3 == 1 && am_or_pm == 'AM'){
-    document.getElementById('t1').innerHTML = c2 +  " " +'AM'
-    document.getElementById('t2').innerHTML = c3 +  " " +'PM'
-    document.getElementById('t3').innerHTML = c4 +  " " +'PM'
-    document.getElementById('t4').innerHTML = c5 + " " + 'PM'
-  }
-  else if(c4 == 1 && am_or_pm == 'AM'){
-    document.getElementById('t1').innerHTML = c2 +  " " +'AM'
-    document.getElementById('t2').innerHTML = c3 +  " " +'AM'
-    document.getElementById('t3').innerHTML = c4 +  " " + 'PM'
-    document.getElementById('t4').innerHTML = c5 +  " " + 'PM' 
-  }
-  else if(c4 == 1 && am_or_pm == 'PM'){
-    document.getElementById('t1').innerHTML = c2 +  " " + 'PM'
-    document.getElementById('t2').innerHTML = c3 +  " " +'PM'
-    document.getElementById('t3').innerHTML = c4 +  " " +'AM'
-    document.getElementById('t4').innerHTML = c5 +  " " +'AM' 
-  }
-  else if(c5 == 1 && am_or_pm == 'AM'){
-    document.getElementById('t1').innerHTML = c2 +  " " +'AM'
-    document.getElementById('t2').innerHTML = c3 +  " " +'AM'
-    document.getElementById('t3').innerHTML = c4 +  " " +'AM'
-    document.getElementById('t4').innerHTML = c5 +  " " +'PM' 
-  }
-  else if(c5 == 1 && am_or_pm == 'PM'){
-    document.getElementById('t1').innerHTML = c2 + " " + 'PM'
-    document.getElementById('t2').innerHTML = c3 + " " +'PM'
-    document.getElementById('t3').innerHTML = c4 + " " +'PM'
-    document.getElementById('t4').innerHTML = c5 + " " +'AM' 
-  }
-  else{
-    document.getElementById('t1').innerHTML = c2 + " " + am_or_pm
-    document.getElementById('t2').innerHTML = c3 + " " + am_or_pm
-    document.getElementById('t3').innerHTML = c4 + " " + am_or_pm
-    document.getElementById('t4').innerHTML = c5 + " " + am_or_pm
-  }
-}
 var inherit_city;
-child_index.prototype = Object.create(parent_index.prototype);
+//child_index.prototype = Object.create(parent_index.prototype);
 /**
  * @desc when the city is selected from the drop down
  */
